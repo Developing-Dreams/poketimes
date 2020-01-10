7014dd3afa03599e5a73b7612a32fc0de1df5267
Prabu Thirumoorthi <pthirumoorthi@altimetrik.com>
2020-01-10 21:30:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import Navbar from './components/Navbar'
+import Home from './components/Home'
+import About from './components/About'
+import Contact from './components/Contact'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { BrowserRouter,Route } from "react-router-dom";
+
+class App extends Component {
+  state = {
+    todos: [
+      {
+        id: 1,
+        content: 'Buy some milk'
+      },
+      {
+        id: 2,
+        content: 'Play cricket'
+      }
+    ]
+  }
+
+  render () {
+    return (
+      <BrowserRouter>
+      <div className='App container'>
+       
+        <Navbar></Navbar>
+        <Route path='/' component={Home}/>
+        <Route path='/contact' component={Contact}/>
+        <Route path='/about' component={About}/>
+
+      </div>
+      </BrowserRouter>
+    )
+  }
 }
 
-export default App;
+export default App
